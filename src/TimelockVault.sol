5f51876e6b242b2199fc0b4a818643e5d91d2def
L41TH <laith.elmershati@gmail.com>
2023-04-18 16:26:48 +0200

    add timelock code

--- src/TimelockVault.sol
+++ src/TimelockVault.sol
@@ -4,13 +4,29 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract TimelockVault is Ownable {
-    function deposit() external payable {}
+    uint256 public s_lastDepositTimestamp = 0;
+
+    error TimeLeft(uint256);
+
+    function deposit() external payable {
+        s_lastDepositTimestamp = block.timestamp;
+    }
 
     function balance() external view returns (uint256) {
         return address(this).balance;
     }
 
+    function timeLeft() external view returns (uint256) {
+        uint256 lastDepositTimestamp = s_lastDepositTimestamp;
+        return lastDepositTimestamp + 1 days - block.timestamp;
+    }
+
     function withdraw() external onlyOwner {
+        uint256 lastDepositTimestamp = s_lastDepositTimestamp;
+        if (block.timestamp < lastDepositTimestamp + 1 days) {
+            revert TimeLeft(lastDepositTimestamp + 1 days - block.timestamp);
+        }
+
         (bool hs,) = payable(owner()).call{value: address(this).balance}("");
         require(hs, "The Withdrawal could not be achieved");
     }
--- test/TimelockVault.t.sol
+++ test/TimelockVault.t.sol
@@ -5,7 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/Console.sol";
 import {TimelockVault} from "../src/TimelockVault.sol";
 
-contract CounterTest is Test {
+contract TimelockVaultTest is Test {
     TimelockVault public vault;
 
     // https://ethereum.stackexchange.com/a/136286
@@ -21,24 +21,41 @@ contract CounterTest is Test {
         assertEq(vault.balance(), 100 ether);
     }
 
-    function testWithdrawal() public {
-        vault.deposit{value: 100}();
-        console.log(vault.owner());
-        vault.withdraw();
-        assertEq(vault.balance(), 0);
-    }
+    // function testWithdrawal() public {
+    //     vault.deposit{value: 100}();
+    //     // vm add 24 hours
+    //     vault.withdraw();
+    //     assertEq(vault.balance(), 0);
+    // }
 
-    function testFailWithdrawal() public {
-        vault.deposit{value: 100}();
-        vault.withdraw();
-        assertEq(vault.balance(), 100);
-    }
+    // function testFailWithdrawal() public {
+    //     vault.deposit{value: 100}();
+    //     // vm add 24 hours
+    //     vault.withdraw();
+    //     assertEq(vault.balance(), 100 ether);
+    // }
 
     function testWithdrawalRevert() public {
         vault.deposit{value: 100}();
 
         vm.prank(address(1));
         vm.expectRevert("Ownable: caller is not the owner");
+        vault.withdraw();
+    }
+
+    function testWithdrawalTimeRevert() public {
+        vault.deposit{value: 100}();
+
+        bytes4 selector = bytes4(keccak256("TimeLeft(uint256)"));
+        vm.expectRevert(abi.encodeWithSelector(selector, vault.s_lastDepositTimestamp() + 1 days - block.timestamp));
+        vault.withdraw();
+    }
+
+    function testWithdrawalVaultTimeRevert() public {
+        vault.deposit{value: 100}();
+
+        bytes4 selector = bytes4(keccak256("TimeLeft(uint256)"));
+        vm.expectRevert(abi.encodeWithSelector(selector, vault.timeLeft()));
         vault.withdraw();
     }
 }
