80eabe6e40922c56a22e9e61d5820b5e1b459353
L41TH <laith.elmershati@gmail.com>
2023-04-20 19:09:39 +0200

    naming convention private variable

--- src/TimelockVault.sol
+++ src/TimelockVault.sol
@@ -5,11 +5,11 @@ import "./interfaces/ITimelockVault.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract TimelockVault is Ownable, ITimelockVault {
-    uint256 private s_lastWithdrawalRequestTimestamp = 0;
-    bool private s_isPendingWithdrawalRequest = false;
+    uint256 private _s_lastWithdrawalRequestTimestamp = 0;
+    bool private _s_isPendingWithdrawalRequest = false;
 
     function deposit() external payable {
-        s_isPendingWithdrawalRequest = false; // reset withdrawal request
+        _s_isPendingWithdrawalRequest = false; // reset withdrawal request
         emit Deposit(block.timestamp, msg.value);
     }
 
@@ -18,9 +18,9 @@ contract TimelockVault is Ownable, ITimelockVault {
     }
 
     function getWithdrawalRequestData() external view returns (bool, uint256, uint256) {
-        uint256 lastWithdrawalRequestTimestamp = s_lastWithdrawalRequestTimestamp;
+        uint256 lastWithdrawalRequestTimestamp = _s_lastWithdrawalRequestTimestamp;
         return (
-            s_isPendingWithdrawalRequest,
+            _s_isPendingWithdrawalRequest,
             lastWithdrawalRequestTimestamp,
             lastWithdrawalRequestTimestamp + 1 days > block.timestamp
                 ? lastWithdrawalRequestTimestamp + 1 days - block.timestamp
@@ -29,23 +29,23 @@ contract TimelockVault is Ownable, ITimelockVault {
     }
 
     function withdrawalRequest() external onlyOwner {
-        s_isPendingWithdrawalRequest = true;
-        s_lastWithdrawalRequestTimestamp = block.timestamp;
+        _s_isPendingWithdrawalRequest = true;
+        _s_lastWithdrawalRequestTimestamp = block.timestamp;
         emit WithdrawalRequest(block.timestamp);
     }
 
     function revokeWithdrawalRequest() external onlyOwner {
-        s_isPendingWithdrawalRequest = false;
+        _s_isPendingWithdrawalRequest = false;
         emit RevokeWithdrawalRequest(block.timestamp);
     }
 
     function withdraw() external onlyOwner isPendingWithdrawalRequest {
-        uint256 lastWithdrawalRequestTimestamp = s_lastWithdrawalRequestTimestamp;
+        uint256 lastWithdrawalRequestTimestamp = _s_lastWithdrawalRequestTimestamp;
         if (block.timestamp < lastWithdrawalRequestTimestamp + 1 days) {
             revert TimeLeft(lastWithdrawalRequestTimestamp + 1 days - block.timestamp);
         }
 
-        s_isPendingWithdrawalRequest = false;
+        _s_isPendingWithdrawalRequest = false;
 
         emit Withdraw(block.timestamp, address(this).balance);
 
@@ -54,7 +54,7 @@ contract TimelockVault is Ownable, ITimelockVault {
     }
 
     modifier isPendingWithdrawalRequest() {
-        require(s_isPendingWithdrawalRequest, "No pending withdrawal request");
+        require(_s_isPendingWithdrawalRequest, "No pending withdrawal request");
         _;
     }
 }
